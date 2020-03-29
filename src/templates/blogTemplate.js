15d2c5c5952159655b9681a237348e55d7c641f6
khxia <17xiak124268@gmail.com>
2020-03-29 10:05:56 -0700

    edited image paths

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -28,7 +28,6 @@ export default function Template({data,}) {
             }}
             dangerouslySetInnerHTML={{__html: html }}
           />
-            
             </p>
         </div>
     </div>
