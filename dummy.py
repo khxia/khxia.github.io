3729c4de12ccf4d5fdaff73f3445495c820f617b
khxia <17xiak124268@gmail.com>
2022-02-10 20:01:29 -0800

    add PR action

--- dummy.py
+++ dummy.py
@@ -1,1 +1,36 @@
-print("This is a dummy script for testing")
+import requests
+import json
+import sys
+import os
+
+# User whitelist and issue number we are looking for
+user_whitelist = ['jodymlin', 'khxia', 'jakobreinwald', 'christinatong01', 'milesswu']
+# HOTH repository issues accessed via Github API
+repo_issues = "https://api.github.com/repos/khxia/khxia.github.io/issues"
+
+issue_num = '5'
+
+url = repo_issues + '/' + issue_num + '/' + 'comments'
+
+# Get given issue
+announcements = requests.get(url)
+print("Request completed with status code: ", announcements.status_code)
+announcements_json = announcements.json()
+
+valid_comments = []
+
+# Keep track of every comment from valid usernames
+for index, element in enumerate(announcements_json):
+    if element['user']['login'] in user_whitelist:
+        body = element['body']
+        subject, partition, comment = body.partition('(Subject) ')
+        comment = {'id': index, 'subject': subject, 'body': comment, 'timestamp': element['created_at']}
+        valid_comments.append(comment)
+
+# Insert all valid comment json objects into a file
+filename = './announcements.json'
+json_string = json.dumps(valid_comments, indent=4)
+with open(filename, 'w') as f:
+    f.write(json_string)
+
+print("Finished.")
